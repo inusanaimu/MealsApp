57808fca6e7c8355f64761e60792d14bc85d906c
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-02-28 09:33:54 +0000

    Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Favorites from './components/Favorites';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Search from './components/Search';
 
 function App() {
-  return (
+  return ( <main>
+
     <Favorites />
+    <Meals/>
+    <Modal/>
+    <Search/>
+
+  </main>
   );
 }
 

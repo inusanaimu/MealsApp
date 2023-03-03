e933a98ef8e74d4d13e5c4f47766e1ef4818a5ab
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-03-03 11:37:02 +0000

    context.js

--- src/context.js
+++ src/context.js
@@ -1,9 +1,23 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 
 const AppContext = React.createContext();
 
-
 const AppProvider = ({children}) => {
+    
+    const fetchData = async () =>{
+        try{
+            const res = await fetch('https://randomuser.me/api/')
+            const data = await res.json()
+            console.log(data)
+        } catch (error){
+            console.log(error)
+        }
+    }
+    
+    useEffect(()=>{
+        fetchData()
+    },[])
+
     return <AppContext.Provider value={{name:'john', role:'student'}}>
         {children}
         </AppContext.Provider>

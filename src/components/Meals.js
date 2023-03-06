f74fe87d6d529a47938dc1a7156719c2ba73436a
Inusa Naimu <62981083+inusanaimu@users.noreply.github.com>
2023-03-06 15:53:15 +0000

    Meals.js

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -9,7 +9,7 @@ const Meals = () => {
      const {idmeal, strMeal:title, strMealThumb:image} = eachMeal;
     
     return <article className='single-meal' key={idmeal}>
-      <img src={image} className='img' style={{width:'200px'}}/>
+      <img src={image} className='img' />
       <footer>
         <h5>{title}</h5>
         <button className='like-btn'>click me</button>
